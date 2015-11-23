03a7dff9183c9d6b215b7a35c541f8af6fa12d72
Thomas Riccioli <thomas.riccioli@gmail.com>
2015-11-23 18:31:19 +0200

    Creation of the Object Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+	login = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
+
+class Gender(models.Model):
+	gender_name = models.CharField(max_length=200)
+
+class Movie(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	image_url = models.CharField(max_length=200)
+	gender = models.ForeignKey(Gender)
+	release_date = models.DateTimeField('date released')
+
+class FavoriteMovie(models.Model):
+	user = models.ForeignKey(User)
+	movie = models.ForeignKey(Movie)
+
+class WatchedMovie(models.Model):
+	user = models.ForeignKey(User)
+	movie = models.ForeignKey(Movie)
+
+class WishedMovie(models.Model):
+	user = models.ForeignKey(User)
+	movie = models.ForeignKey(Movie)
